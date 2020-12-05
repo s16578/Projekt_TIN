f59025233643e0f685248e530954eb313f63ad09
Adam Daniluk <s16578@pjwstk.edu.pl>
2020-12-05 21:49:40 +0100

    Updated database model, updated repair, computer, employee pages.

--- Database/gwarancje_model_create.sql
+++ Database/gwarancje_model_create.sql
@@ -1,5 +1,5 @@
 -- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2020-11-22 14:29:18.181
+-- Last modification date: 2020-12-03 19:55:27.386
 
 -- tables
 -- Table: Computer
@@ -15,18 +15,6 @@ CREATE TABLE Computer (
     CONSTRAINT Computer_pk PRIMARY KEY  (id)
 );
 
--- Table: Employee_repair
-CREATE TABLE Employee_repair (
-    id int  NOT NULL,
-    employee_id int  NOT NULL,
-    repair_id int  NOT NULL,
-    date_start date  NOT NULL,
-    date_end date  NULL,
-    note nvarchar(200)  NULL,
-    cost int  NOT NULL,
-    CONSTRAINT Employee_repair_pk PRIMARY KEY  (id)
-);
-
 -- Table: Employees
 CREATE TABLE Employees (
     id int  NOT NULL,
@@ -39,8 +27,12 @@ CREATE TABLE Employees (
 -- Table: Repairs
 CREATE TABLE Repairs (
     id int  NOT NULL,
+    employee_id int  NOT NULL,
     computer_id int  NOT NULL,
-    status nvarchar(20)  NOT NULL,
+    date_start date  NOT NULL,
+    date_end date  NULL,
+    note nvarchar(200)  NULL,
+    status nvarchar(100)  NOT NULL,
     cost int  NULL,
     CONSTRAINT Repairs_pk PRIMARY KEY  (id)
 );
@@ -51,15 +43,10 @@ ALTER TABLE Repairs ADD CONSTRAINT Computer_Repairs
     FOREIGN KEY (computer_id)
     REFERENCES Computer (id);
 
--- Reference: Employee_repair_Employees (table: Employee_repair)
-ALTER TABLE Employee_repair ADD CONSTRAINT Employee_repair_Employees
-    FOREIGN KEY (repair_id)
+-- Reference: Employee_repair_Employees (table: Repairs)
+ALTER TABLE Repairs ADD CONSTRAINT Employee_repair_Employees
+    FOREIGN KEY (employee_id)
     REFERENCES Employees (id);
-
--- Reference: Repairs_Employee_repair (table: Employee_repair)
-ALTER TABLE Employee_repair ADD CONSTRAINT Repairs_Employee_repair
-    FOREIGN KEY (repair_id)
-    REFERENCES Repairs (id);
 
 -- End of file.
 
